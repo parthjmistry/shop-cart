470f882adf6f838eb69af5ea1debc82098971a15
Mayank Patel <mayank@aspen-it.com>
2022-02-16 15:20:56 +0530

    cart changes

--- shop-cart/src/app/Cart/cart-items/cart-items.component.ts
+++ shop-cart/src/app/Cart/cart-items/cart-items.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CartModel } from '../Models/cart-model';
+import { CartServiceService } from '../Services/cart-service.service';
 
 @Component({
   selector: 'app-cart-items',
   templateUrl: './cart-items.component.html',
-  styleUrls: ['./cart-items.component.css']
+  styleUrls: ['./cart-items.component.css'],
 })
 export class CartItemsComponent implements OnInit {
+  Products: CartModel[] = [];
+  NewProduct: CartModel = new CartModel();
 
-  constructor() { }
+  // Cart: CartModel[] = [];
+
+  constructor(
+    private router: Router,
+    private CartService: CartServiceService
+  ) {}
 
   ngOnInit(): void {
+    this.CartService.Products$.subscribe((res) => {
+      this.Products = res;
+      console.log('----------------------------');
+      console.log(res);
+    });
   }
 
+  AddToCart() {
+    this.CartService.AddItemToCart(this.NewProduct);
+  }
+
+  EditItem(Action: string, ProdutId: number) {
+    if (Action === 'Add') {
+      this.Products.filter((List) => List.Id === ProdutId).map(
+        (List) => ((List.Qty += 1), (List.Amount = List.Price * List.Qty))
+      );
+    } else {
+      this.Products.filter((List) => List.Id === ProdutId).map(
+        (List) => (
+          List.Qty == 0 ? 0 : (List.Qty -= 1),
+          (List.Amount = List.Price * List.Qty)
+        )
+      );
+    }
+  }
+
+  ClearCart() {
+    this.Products = [];
+    this.router.navigateByUrl('cart');
+  }
 }
