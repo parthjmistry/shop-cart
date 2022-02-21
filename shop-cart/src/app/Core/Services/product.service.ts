e5a072a38a99693135f7e53e7b37bc4049a1cbba
Hitesh Bavaliya <hjbavaliya@gmail.com>
2022-02-21 10:17:33 +0530

    admin - add product

--- shop-cart/src/app/Core/Services/product.service.ts
+++ shop-cart/src/app/Core/Services/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, from, Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, from, Observable, of, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { Product } from '../Models/ProductModel';
 
 @Injectable({
@@ -14,8 +14,21 @@ export class ProductService {
   colorName = new BehaviorSubject<string>('');
   jsonfilePath = 'assets/products.json';
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
   constructor(private http: HttpClient) { }
   
+
+  addProduct(product:Product): Observable<Product> {
+    return this.http.post<Product>(this.jsonfilePath, JSON.stringify(product), this.httpOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    )
+  }
    
   getProductData(): Observable<Product[]> {
     return this.http.get<Product[]>(this.jsonfilePath);
@@ -46,4 +59,19 @@ export class ProductService {
       map((data) => data.filter((res) => res.id === pid))
     );
   }
+
+  errorHandler(error:any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+  
+  
 }
