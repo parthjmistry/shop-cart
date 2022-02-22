1cac501f06ea38e120f02a07ae0b38bb54005234
Mayank Patel <mayank@aspen-it.com>
2022-02-22 12:01:31 +0530

    cart Changes

--- shop-cart/src/app/Cart/cart/cart.component.ts
+++ shop-cart/src/app/Cart/cart/cart.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CartServiceService } from '../Services/cart-service.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cartItemCount: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _cartService: CartServiceService) {
+    console.log('--------cart-----------');
+    this._cartService.cartItemCount.subscribe((itemCount) => {
+      this.cartItemCount = itemCount;
+      console.log(this.cartItemCount);
+    });
   }
 
+  ngOnInit(): void {}
 }
