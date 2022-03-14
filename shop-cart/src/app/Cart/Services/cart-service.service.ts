5cdf7b67a447c9027c1eadba0a2caebcd3ac390d
Mayank Patel <mayank@aspen-it.com>
2022-03-14 17:33:18 +0530

    billing details

--- shop-cart/src/app/Cart/Services/cart-service.service.ts
+++ shop-cart/src/app/Cart/Services/cart-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
-import { CartModel } from '../Models/cart-model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,10 +16,7 @@ export class CartServiceService {
 
     const currentArray = JSON.parse(localStorage.getItem('cartItem') || '{}');
 
-    console.log(currentArray);
-
     if (currentArray.length != undefined) {
-      console.log('currentArray');
       for (let i = 0; i < currentArray.length; ++i) {
         this.TotalCartAmt += currentArray[i].Qty * currentArray[i].price;
       }
@@ -33,38 +29,5 @@ export class CartServiceService {
 
   constructor() {
     this.setCartItemCount();
-  }
-
-  private _Cart = new BehaviorSubject<CartModel[]>([]);
-  readonly Products$ = this._Cart.asObservable();
-
-  private Products: CartModel[] = [];
-  private nextId = 0;
-
-  GetCartItems() {
-    this._Cart.next(this.Products);
-  }
-
-  AddItemToCart(item: CartModel) {
-    debugger;
-    let newId = ++this.nextId;
-    console.log(newId);
-    item.Id = newId;
-    item.Name = 'Product ' + newId;
-    item.Qty = 1;
-    item.Price = newId * 100;
-    item.Amount = item.Qty * item.Price;
-
-    this.Products.push(item);
-    this._Cart.next(this.Products);
-  }
-
-  RemoveItemFromCart(id: number) {
-    this.Products.forEach((t, i) => {
-      if (t.Id === id) {
-        this.Products.splice(i, 1);
-      }
-      this._Cart.next(Object.assign([], this.Products));
-    });
   }
 }
--- shop-cart/src/app/Checkout/checkout/checkout.component.ts
+++ shop-cart/src/app/Checkout/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BillingDetailsComponent } from '../billing-details/billing-details.component';
 
 @Component({
   selector: 'app-checkout',
@@ -10,6 +11,11 @@ export class CheckoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  PlaceOrder(){
+
+
+  }
 }
