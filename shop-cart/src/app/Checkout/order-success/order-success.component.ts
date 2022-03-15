6a414e1715445f27d1f45c09f5679a40df8b0620
Mayank Patel <mayank@aspen-it.com>
2022-03-15 16:55:04 +0530

    Order Success

--- shop-cart/src/app/Checkout/order-success/order-success.component.ts
+++ shop-cart/src/app/Checkout/order-success/order-success.component.ts
@@ -5,18 +5,17 @@ import { CartServiceService } from 'src/app/Cart/Services/cart-service.service';
 @Component({
   selector: 'app-order-success',
   templateUrl: './order-success.component.html',
-  styleUrls: ['./order-success.component.css']
+  styleUrls: ['./order-success.component.css'],
 })
 export class OrderSuccessComponent implements OnInit {
   billingDetails: any;
 
-  constructor(private _cartService: CartServiceService) { }
+  constructor(private _cartService: CartServiceService) {}
 
   ngOnInit(): void {
     this.billingDetails = this._cartService.UserBillingDetails.subscribe(
       (data) => {
-        this.billingDetails = JSON.stringify(data);
-        console.log(data);
+        this.billingDetails = data;
         console.log(this.billingDetails);
       }
     );
